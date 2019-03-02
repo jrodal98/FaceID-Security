ec90d2e95be4738e950635cc740dd1a8e2452b74
Jake Rodal <jrodal98@gmail.com>
2019-03-02 17:16:28 -0500

    update

--- src/webcam.py
+++ src/webcam.py
@@ -4,6 +4,9 @@ import cv2
 
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
+vc.set(cv2.CAP_PROP_FRAME_WIDTH, 1200)
+vc.set(cv2.CAP_PROP_FRAME_HEIGHT, 1200)
+# cv2.set(cv2.CAP_PROP_FPS, fps)
 
 if vc.isOpened():  # try to get the first frame
     rval, frame = vc.read()
@@ -11,7 +14,8 @@ else:
     rval = False
 
 while rval:
-    cv2.imshow("preview", frame)
+    im = cv2.imread("test_output.jpg")
+    cv2.imshow("preview", im)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
     if key == 27:  # exit on ESC
